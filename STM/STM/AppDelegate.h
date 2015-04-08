0f3e457a9f12b8a6f53f4f380643f0f0b6480229
Brandon <baplaste@gmail.com>
2015-04-08 12:56:45 -0400

    Initial xcode project

---
+++ STM/STM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  STM
+//
+//  Created by Brandon Plaster on 4/8/15.
+//  Copyright (c) 2015 BrandonPlaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ STM/STM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  STM
+//
+//  Created by Brandon Plaster on 4/8/15.
+//  Copyright (c) 2015 BrandonPlaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
